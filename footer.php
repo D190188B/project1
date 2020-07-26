9b53412b0042eb9fd3ec6133609faacb914cc7da
D190188B <68429213+D190188B@users.noreply.github.com>
2020-07-26 12:06:35 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,11 +1,9 @@
-<head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.staticfile.org/font-awesome/4.7.0/css/font-awesome.css">
-</head>
-    <footer class="container py-5" id="footer">
+
+    <footer class="container py-5" id="footer" >
+                    
                     <div class="row" >
-                
-                      <div class="col-6 col-md">
+
+                      <div class="col-12 col-md">
                         <h5>About us</h5>
                         <ul class="list-unstyled text-small">
                           <li><a class="text-muted" href="#"></a></li>
@@ -28,11 +26,11 @@
                       <div class="col-6 col-md">
                         <h5>Our therapists</h5>
                         <ul class="list-unstyled text-small">
-                          <li><a class="text-muted" href="adidasLifestyle.php">Therapists information</a></li>
-                          <li><a class="text-muted" href="adidasRunning.php">Apply Job</a></li>
+                          <li><a class="text-muted" href="#">Therapists information</a></li>
+                          <li><a class="text-muted" href="#">Apply Job</a></li>
                       </div>
 
-                      <div class="col-6 col-md">
+                      <div class="col-6 col-md col-md">
                         <small class="d-block mb-3 text-muted">&copy;2020 Southern IT-19B</small>
                       </div>
                       
