de6cfda52473d9bac35b9c734d710d4bf2dcd148
D190188B <68429213+D190188B@users.noreply.github.com>
2020-08-31 16:57:02 +0800

    Update Appointment.php

--- Appointment.php
+++ Appointment.php
@@ -12,9 +12,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-
-
-
 $email = $_SESSION['email']; //get user email
 $sql = "SELECT * FROM select_question WHERE user_email='$email'"; //get the id from select_question
 $result = $conn->query($sql) or die($conn->error . __LINE__);
@@ -35,7 +32,46 @@ $query = "select * from user_choices where selectID='$generateid'";
 $choices = $conn->query($query) or die($conn->error . __LINE__);
 $choice = $choices->fetch_assoc();
 
+if (isset($_SESSION['work_id'])) {
 
+    if (isset($_SESSION['id'])) {
+
+        if (!empty($_SESSION['work_id'])) {
+            $therapist_id = $_SESSION['work_id'];
+            $sql = "SELECT * FROM therapist where therapist_id='$therapist_id'"; //id is database name
+            $result = $conn->query($sql) or die($conn->error . __LINE__);
+
+            if ($result->num_rows > 0) { //over 1 database(record) so run
+                while ($row = $result->fetch_assoc()) {
+                    //display result
+                    $select_id = $row['therapist_id']; //[] inside is follow database 
+                    $select_name_first = $row['name_first'];
+                    $select_name_last = $row['name_last'];
+                    $select_email = $row['email'];
+                    $select_gender = $row['gender'];
+                    $select_age = $row['age'];
+                    $select_phone = $row['phone'];
+                    $select_ic = $row['ic'];
+                    $select_address = $row['address'];
+                    $select_therapist_city = $row['therapist_city'];
+                    $select_therapist_postCode = $row['therapist_postCode'];
+                    $select_therapist_state = $row['therapist_state'];
+                    $select_license = $row['license'];
+                    $select_resume = $row['resume'];
+                    $select_profile_image = $row['profile_image'];
+                    $select_statusID = $row['statusID'];
+                    $select_created_at = $row['created_at'];
+                }
+            }
+
+            $sqli = "select * from select_question where user_email='$email'";
+            $run = $conn->query($sqli) or die($conn->error . __LINE__);
+            $result1 = $run->fetch_assoc();
+        }
+    } else {
+        echo '<script>window.alert("You must login first..!!");window.location.assign("login.php")</script>';
+    }
+}
 
 if (isset($_GET['id'])) { //get therapist id
     $email = $_SESSION['email'];
@@ -58,7 +94,6 @@ if (isset($_GET['id'])) { //get therapist id
     }
 
 
-
     $sqli = "select * from select_question where user_email='$email'";
     $run = $conn->query($sqli) or die($conn->error . __LINE__);
     $result1 = $run->fetch_assoc();
@@ -72,11 +107,11 @@ if (isset($_POST['upload'])) { //upload appointment
     $date = $_POST['date'];
     $therapistID = $_POST['therapistID'];
 
-    $sql = "INSERT INTO `appointment` VALUES('$generateid','$email','$method','$time','$date','$therapistID',NOW())";
+    $sql = "INSERT INTO `appointment` VALUES('$generateid','$email','$method','$time','$date','$therapistID',1,1,NOW())";
     $result = $conn->query($sql) or die($conn->error . __LINE__);
 
     if ($result == true) {
-        echo '<script>window.alert("Successful...!");window.location.assign("profile.php")</script>';
+        echo '<script>window.alert("Successful...!");window.location.assign("profileCopy.php")</script>';
     }
 }
 
@@ -116,12 +151,12 @@ if (isset($_POST['upload'])) { //upload appointment
                                 <div class="row">
                                     <div class="col-md-6">
                                         <h5 style="color:white">Email</h5>
-                                        <input type="text" name="email" value="<?php echo $_SESSION['email'] ?>" readonly>
+                                        <input type="text" class="form-control" name="email" value="<?php echo $_SESSION['email'] ?>" readonly>
                                     </div>
 
                                     <div class="col-md-6">
                                         <h5 style="color:white">Method</h5>
-                                        <select id="method" name="method" require>
+                                        <select id="method" name="method" class="custom-select" required>
                                             <option value="phone">Phone</option>
                                             <option value="online chat">Online Chat</option>
 
@@ -132,7 +167,7 @@ if (isset($_POST['upload'])) { //upload appointment
 
                                 <div class="row">
                                     <div class="col-md-6">
-                                        <h5 style="color:white">Time</h5>
+                                        <h5 style="color:white">Date and Time</h5>
                                         <input type="time" name="time" placeholder="Time" required>
                                     </div>
 
@@ -147,14 +182,30 @@ if (isset($_POST['upload'])) { //upload appointment
                                         <h5 style="color:white">Therapist</h5>
                                         <div class="row" style="padding:20px;border:1px solid white;">
                                             <div class="col-md-6" align="center">
-                                                <img src="<?php echo $profile_image ?>" alt="img">
+                                                <img src="<?php if ((isset($_SESSION['work_id'])) && (!empty($_SESSION['work_id']))) {
+                                                                echo $select_profile_image;
+                                                            } else if (isset($_GET['id'])) {
+                                                                echo $profile_image;
+                                                            } ?>" alt="img">
                                                 <?php $appointmentID = $result1['generate_id'] ?>
                                             </div>
 
                                             <div class="col-md-6">
-                                            <input type="hidden" name="therapistID" value="<?php echo $id ?>">
-                                            <h3 class="therapist_name"><?php echo $name_first . "&nbsp;" . $name_last ?></h3>
-                                            <h3 class="therapist_license"><?php echo $license ?></h3>
+                                                <input type="hidden" name="therapistID" value="<?php if ((isset($_SESSION['work_id'])) && (!empty($_SESSION['work_id']))) {
+                                                                                                    echo $select_id;
+                                                                                                } else if (isset($_GET['id'])) {
+                                                                                                    echo $id;
+                                                                                                } ?>">
+                                                <h3 class="therapist_name"><?php if ((isset($_SESSION['work_id'])) && (!empty($_SESSION['work_id']))) {
+                                                                                echo $select_name_first . "&nbsp;" . $select_name_last;
+                                                                            } else if (isset($_GET['id'])) {
+                                                                                echo $name_first . "&nbsp;" . $name_last;
+                                                                            } ?></h3>
+                                                <h3 class="therapist_license"><?php if ((isset($_SESSION['work_id'])) && (!empty($_SESSION['work_id']))) {
+                                                                                    echo $select_license;
+                                                                                } else if (isset($_GET['id'])) {
+                                                                                    echo $license;
+                                                                                } ?></h3>
                                             </div>
                                         </div>
                                     </div>
