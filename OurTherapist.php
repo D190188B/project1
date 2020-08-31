cc9eccd44bbfd6ec0efe814f815952dcaa3d6c17
D190188B <68429213+D190188B@users.noreply.github.com>
2020-08-31 16:57:50 +0800

    Update OurTherapist.php

--- OurTherapist.php
+++ OurTherapist.php
@@ -12,122 +12,252 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-session_start();
-?>
 
+mysqli_set_charset($conn, 'utf8');
+
+
+//check the pathinfo and upload the file
+function upload_profile($path, $file)
+{
+    $targetDir = $path;
+    $default = "beard.png";
+
+    // get the filename
+    $filename = basename($file['name']);
+    $targetFilePath = $targetDir . $filename;
+    $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
+
+    if (!empty($filename)) {
+        // allow certain file format
+        $allowType = array('jpg', 'png', 'jpeg', 'gif', 'pdf');
+        if (in_array($fileType, $allowType)) {
+            // upload file to the server
+            if (move_uploaded_file($file['tmp_name'], $targetFilePath)) {
+                return $targetFilePath;
+            }
+        }
+    }
+    // return default image
+    return $path . $default;
+}
+
+session_start();
+
+//prevent injection
+function validate_input_text($textValue)
+{
+    if (!empty($textValue)) {
+        $trim_text = trim($textValue);
+        // remove illegal character
+        $sanitize_str = filter_var($trim_text, FILTER_SANITIZE_STRING);
+        return $sanitize_str;
+    }
+    return '';
+}
+
+//prevent injection
+function validate_input_email($emailValue)
+{
+    if (!empty($emailValue)) {
+        $trim_text = trim($emailValue);
+        // remove illegal character
+        $sanitize_str = filter_var($trim_text, FILTER_SANITIZE_EMAIL);
+        return $sanitize_str;
+    }
+    return '';
+}
+
+
+
+if (isset($_POST['submit'])) { //if user register an account
+    // error variable.
+    $error = array();
+
+    $birth = $_POST['birth'];
+
+    $firstname = validate_input_text($_POST['firstname']);
+    if (empty($firstname)) {
+        $error[] = "You forgot to enter your Name";
+    }
+
+    $lastname = validate_input_text($_POST['lastName']);
+    if (empty($lastname)) {
+        $error[] = "You forgot to enter your Name";
+    }
+
+    $email = validate_input_text($_POST['email']);
+    if (empty($email)) {
+        $error[] = "You forgot to enter your Email";
+    }
+
+    $phone = validate_input_text($_POST['phone']);
+    if (empty($phone)) {
+        $error[] = "You forgot to enter your phone number";
+    }
+
+    $address = mysqli_real_escape_string($conn, $_POST['address']);
+    if (empty($address)) {
+        $error[] = "You forgot to enter your address";
+    }
+
+    $city = validate_input_text($_POST['city']);
+    if (empty($city)) {
+        $error[] = "You forgot to enter your city";
+    }
+
+    $state = validate_input_text($_POST['state']);
+    if (empty($state)) {
+        $error[] = "You forgot to enter your state";
+    }
+
+    $postCode = validate_input_text($_POST['postCode']);
+    if (empty($postCode)) {
+        $error[] = "You forgot to enter your post code";
+    }
+
+    $password = validate_input_text($_POST['password']);
+    if (empty($password)) {
+        $error[] = "You forgot to enter your password";
+    }
+
+    $confirm_pwd = validate_input_text($_POST['confirm_pwd']);
+    if (empty($confirm_pwd)) {
+        $error[] = "You forgot to enter your Confirm Password";
+    }
+
+
+    $files = $_FILES['profileUpload'];
+    $profileImage = upload_profile('./images/profile/', $files);
+
+
+    if (empty($error) && ($password == $confirm_pwd)){
+        $generateid = uniqid();
+        // register a new user
+        $hashed_pass = password_hash($password, PASSWORD_DEFAULT);
+
+        $sqli = "Select * from client where email='$email'"; //username and password same ？
+        $result1 = mysqli_query($conn, $sqli) or die($conn->error.__LINE__); //sql
+
+        $count = mysqli_num_rows($result1);
+
+        if ($count == 0) {
+            $sql = "insert into client values('$generateid','$firstname','$lastname','$birth','$phone','$address','$city','$postCode','$state','$email','$hashed_pass','$profileImage',NOW())";
+            $result = $conn->query($sql) or die($conn->error.__LINE__);
+
+            if ($result == true) {
+                echo '<script>window.alert("Register successful...!")</script>';
+            }
+        } else {
+            echo '<script>window.alert("This email has already registered...!")</script>';
+        }
+    } else {
+        echo '<script>window.alert("Password is not match....!!!!")</script>';
+    }
+}
+
+
+
+if (isset($_POST['login'])) { //if user login
+    $error = array();
+
+    $email = validate_input_email($_POST['email']);
+    if (empty($email)) {
+        $error[] = "You forgot to enter your Email";
+    }
+
+    $password = validate_input_text($_POST['password']);
+    if (empty($password)) {
+        $error[] = "You forgot to enter your password";
+    }
+
+
+    if (empty($error)) {
+        // sql query
+        $sql = "select * from client where email='$email'"; //username and password same ？
+        $result = $conn->query($sql) or die($conn->error.__LINE__);
+
+
+        if ($result->num_rows > 0) { //over 1 database(record) so run
+            while ($row = $result->fetch_assoc()) {
+                //display result
+                $id = $row['id']; //[] inside is follow database 
+                $passwordHash = $row['password'];
+            }
+            if (password_verify($password, $passwordHash)) {
+                $_SESSION['id'] = $id;
+                header('refresh: 0; url=Home.php');
+                exit();
+            } else {
+                echo '<script>window.alert("Unavariable name or password")</script>';
+            }
+        }
+    } else {
+        echo '<script>window.alert（"Please Fill out email and password to login!"）</script>';
+    }
+}
+?>
 <!doctype html>
 <html lang="en">
 
 <head>
-    <title>Our Therapist</title>
-    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    <style>
+    </style>
 </head>
 <header>
     <?php require_once("header1.php"); ?>
 </header>
+<!-- registration area -->
 
-<section id="ourtherapist">
+<body>
+    <section id="login-form">
 
-    <body>
         <div class="container">
-            <!-- page-header -->
-
-            <div class="page-header">
-                <div class="row">
-                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                        <div class="page-caption">
-                            <h1 class="page-title">Meet our therapist</h1>
+            <div class="row m-0">
+                <div class="col-lg-4 offset-lg-4">
+                    <div class="text-center pb-5">
+                        <h1 class="login-title">Login</h1>
+                        <p class="p-1 m-0 font-ubuntu">Login and enjoy additional features</p>
+                    </div>
+                    <div class="upload-profile-image d-flex justify-content-center pb-4">
+                        <div class="text-center">
+                            <img src="images/profile/beard.png" style="width: 200px; height: 200px" class="img rounded-circle" alt="profile">
                         </div>
                     </div>
-                </div>
-            </div>
-            <!-- /.page-header-->
-            <!-- news -->
+                    <div class="d-flex justify-content-center">
+                        <form action="login.php" method="post" enctype="multipart/form-data" id="log-form">
 
-            <div class="card-section">
-                <div class="card-block mb30" style="background-color:rgba(210,232,246);">
-                    <div class="row">
-                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                            <!-- section-title -->
-                            <div class="section-title mb-0">
-                                <h2>Counselor as Friend.</h2>
-                                <p>Our Counselors are licensed, trained, experienced, and accredited psychologists (PhD / PsyD),
-                                    marriage and family therapists (MFT), clinical social workers (LCSW),
-                                    or licensed professional counselors (LPC). All of them have a Masters Degree or a Doctorate Degree
-                                    in their field. They have been qualified and certified by their state professional board after successfully completing the necessary education, exams, training and practice. While their experience, expertise and background vary,
-                                    they all possess at least 3 years and 1,000 hours of hands-on experience. </p>
-                            </div>
-                            <!-- /.section-title -->
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <form action="OurTherapist.php" method="POST">
-                <div class="col-md-12">
-                    <div clas="col-md-12" id="search">
-                        <div class="row">
-                            <input type="search" id="searchText" name="Search" class="form-control mr-sm-2" placeholder="Search">
-                            <button class="btn my-2 my-sm-12" type="submit" id="searchbtn" name="submit"><i class="fas fa-search"></i></button>
-                        </div>
-                    </div>
-            </form>
-
-            <div class="card border-0" style="margin-left: -14px;margin-right:-13px;">
-                <div class="row">
-                    <!-- Show therapist -->
-                    <?php
-                    if (isset($_POST['submit'])) {
-                        $keyword = $_POST['Search'];
-                    }
-
-                    //step:3 for search
-                    $search = "";
-                    if (isset($_POST['Search'])) {
-                        $search = " and name_first like '%" . $keyword . "%' or name_last like '%".$keyword."%'"; //（.）把keyword把search
-                    }
-
-                    $sql = "select * from therapist where statusID='2'" . $search; //有where在sql，search sql 不能再有where 换成and
-                    $result = $conn->query($sql) or die($conn->error.__LINE__); //Define sql, run sql
-                    if ($result->num_rows > 0) { //over 1 database(record) so run
-                        while ($row = $result->fetch_assoc()) {
-                            //display result
-                            $id = $row['therapist_id']; //[] inside is follow database 
-                            $name_first = $row['name_first'];
-                            $name_last = $row['name_last'];
-                            $profile_image = $row['profile_image'];
-                            $license = $row['license'];
-                    ?>
-                            <div class="col-md-4">
-                            <a href="therapistDetail.php?id=<?php echo $id ?>">
-                                <div class="card h-100 border-100">
-                                    <div class="card-body">
-                                            <img src="<?php echo $profile_image ?>" alt="" id="therapist1">
-                                            <h3 class="therapistname"><?php echo $name_first . "&nbsp;" . $name_last ?></h3>
-                                            <h5 class="therapistedu"><?php echo $license ?></h5>   
-                                    </div>
+                            <div class="form-row my-4">
+                                <div class="col">
+                                    <h4>Email</h4>
+                                    <input type="email" required name="email" id="email" class="form-control" value="<?php if(isset($_POST['login'])){echo $_POST['email'];} ?>">
                                 </div>
-                                </a>
                             </div>
 
-                    <?php
-                        } //while
-                    } //if
-                    ?>
-                    <!-- End -->
+                            <div class="form-row my-4">
+                                <div class="col">
+                                    <h4>Password</h4>
+                                    <input type="password" required name="password" id="password" class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="form-row my-0">
+                                <div class="col">
+                                    <p>Forget your password? Click here <a href="#">Reset Password</a></p>
+                                    <span class="text-black-100">Don't have account?</span> &nbsp;<button type="button" class="btn btn-white" data-toggle="modal" data-target="#registerModal" id="register"><strong>Register</strong></button>
+                                </div>
+                            </div>
+
+                            <div class="submit-btn text-center my-3">
+                                <input type="submit" name="login" value="submit">
+                            </div>
+
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-        </div>
-    </body>
-</section>
-<!-- Team -->
-
-
-
-
-<footer>
-    <?php require_once("footer.php"); ?>
-
-</footer>
+    </section>
+</body>
+<?php include("modal.php") ?>
 
 </html>
