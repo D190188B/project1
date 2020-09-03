fec67c13fcacf04cc4ac461502fec63d4edd8ca1
D190188B <68429213+D190188B@users.noreply.github.com>
2020-09-03 21:33:44 +0800

    Update theraProfile.php

--- theraProfile.php
+++ theraProfile.php
@@ -38,6 +38,8 @@ if (isset($_SESSION['thera_id'])) {
             $_SESSION['thera_gender'] = $row['gender'];
         }
     }
+}else{
+    echo '<script>window.alert("You must login first!");window.location.assign("therapistLogin.php")</script>';
 }
 
 $id = $_SESSION['thera_id'];
@@ -176,6 +178,7 @@ if (isset($_POST['submit'])) {
     }
 }
 date_default_timezone_set('Singapore');
+$time = date("h:i a");
 $date = date("Y-m-d");
 
 //display today list
@@ -682,7 +685,7 @@ if (isset($_POST['sub_logout'])) {
                         <div class="UserAppointment" id="UserAppointmentSecond">
                             <i class="fa fa-calendar" aria-hidden="true" id="appointment_iconSecond"></i>
                             <?php if ($AppointmentTotal != 0) {
-                                echo "span class=\"badge badge-danger\" style=\"padding:7px;font-size:20px;\" id=\"appointmentSecond_number\">$AppointmentTotal</span>";
+                                echo "<span class=\"badge badge-danger\" style=\"padding:7px;font-size:20px;\" id=\"appointmentSecond_number\">$AppointmentTotal</span>";
                             } ?>
                         </div>
                         <div class="UserReport" id="UserReportSecond">
@@ -702,7 +705,7 @@ if (isset($_POST['sub_logout'])) {
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-    <script src="js/therapistEdit.js" type="text/javascript"></script>
+    <script src="js/therapist.js" type="text/javascript"></script>
 </body>
 
 </html>
