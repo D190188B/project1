b3e45705a5fba73e2360e870a79af241e3161aff
D190188B <68429213+D190188B@users.noreply.github.com>
2020-09-03 21:32:32 +0800

    Update question.php

--- question.php
+++ question.php
@@ -1,21 +1,6 @@
 <?php
-$servername = "localhost"; //localhost for local PC or use IP address
-$username = "root"; //database name
-$password = ""; //database password
-$database = "oncoun"; //database name
-
-// Create connection #scawx
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Check connection #scawx
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-
+include ("sessionTop.php");
 $generateID = uniqid();
-
-session_start();
 
 //if user get started the question
 if (isset($_GET['id'])) {
