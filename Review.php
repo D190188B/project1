80a634cff59033d32647f535fcc49a7aec6e532d
D190188B <68429213+D190188B@users.noreply.github.com>
2020-09-03 21:29:26 +0800

    Update Review.php

--- Review.php
+++ Review.php
@@ -1,18 +1,5 @@
 <?php
-$servername = "localhost"; //localhost for local PC or use IP address
-$username = "root"; //database name
-$password = ""; //database password
-$database = "oncoun"; //database name
-
-// Create connection #scawx
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Check connection #scawx
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-session_start();
+include ("sessionTop.php");
 ?>
 
 <!doctype html>
@@ -21,21 +8,21 @@ session_start();
 <head>
     <title>Review</title>
     <link rel="stylesheet" type="text/css" href="css/Reviews.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    
+    <style>
+        #reviews {
+            background-image: url('images/twinkle.jpg');
+            background-size: 100% 100%;
+        }
+    </style>
 
 </head>
 
 
 <header>
     <?php require_once("header1.php"); ?>
-
-    <style>
-        #reviews {
-            background-image: url('images/twinkle.jpg');
-            background-size: 100% 100%;
-
-        }
-    </style>
 </header>
 
 <section id="reviews">
