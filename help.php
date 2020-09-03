ab68acdd724f9546697a8b1cec3868c90da2273b
D190188B <68429213+D190188B@users.noreply.github.com>
2020-09-03 21:30:37 +0800

    Update help.php

--- help.php
+++ help.php
@@ -1,18 +1,5 @@
 <?php
-$servername = "localhost"; //localhost for local PC or use IP address
-$username = "root"; //database name
-$password = ""; //database password
-$database = "oncoun"; //database name
-
-// Create connection #scawx
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Check connection #scawx
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-session_start();
+include ("sessionTop.php");
 $sql = "select * from services";
 $result = $conn->query($sql) or die($conn->error . __LINE__);
 
