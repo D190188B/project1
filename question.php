e849242662a753075fe9c2a2539b611956aece76
D190188B <68429213+D190188B@users.noreply.github.com>
2020-09-09 20:22:09 +0800

    Update question.php

--- question.php
+++ question.php
@@ -1,23 +1,36 @@
 <?php
-include ("sessionTop.php");
+include("sessionTop.php");
 $generateID = uniqid();
 
 //if user get started the question
 if (isset($_GET['id'])) {
 
-    if (isset($_SESSION['id'])) {
-        $user_ID = $_SESSION['id'];
+    if (isset($_SESSION['client_id'])) {
+        $user_ID = $_SESSION['client_id'];
         $_SESSION['work_id'] = '';
         $sql = "select * from client where id ='$user_ID'"; //id is database name
-        $result = $conn->query($sql) or die($conn->error . __LINE__);;
+        $result = $conn->query($sql) or die($conn->error . __LINE__);
+
+
+
 
         if ($result->num_rows > 0) { //over 1 database(record) so run
             while ($row = $result->fetch_assoc()) {
-                $_SESSION['email'] = $row['email'];
+                $_SESSION['client_email'] = $row['email'];
             }
         }
-        $sqli = "insert into select_question values('$generateID','" . $_SESSION['email'] . "')";
-        $run = $conn->query($sqli) or die($conn->error . __LINE__);
+
+        $mysqli = "SELECT * FROM appointment where user_Email='" . $_SESSION['client_email'] . "' and appointment_status=1 or appointment_status=2 or appointment_status=5";
+        $check = $conn->query($mysqli) or die($conn->error . __LINE__);
+        $count = mysqli_num_rows($check);
+
+
+        if ($count > 0) {
+            echo '<script>window.alert("You must finish the current consultation first!");window.location.assign("Help.php")</script>';
+        } else {
+            $sqli = "insert into select_question values('$generateID','" . $_SESSION['client_email'] . "')";
+            $run = $conn->query($sqli) or die($conn->error . __LINE__);
+        }
     } else {
         $_SESSION['work_id'] = '';
         echo '<script>window.alert("You must login first!");window.location.assign("login.php")</script>';
@@ -26,21 +39,33 @@ if (isset($_GET['id'])) {
 
 if (isset($_GET['work_id'])) {
 
-    if (isset($_SESSION['id'])) {
-        $user_ID = $_SESSION['id'];
+    if (isset($_SESSION['client_id'])) {
+        $user_ID = $_SESSION['client_id'];
         $work_id = $_GET['work_id'];
         $_SESSION['work_id'] = $work_id;
 
+
+
         $sql = "select * from client where id ='$user_ID'"; //id is database name
-        $result = $conn->query($sql) or die($conn->error . __LINE__);;
+        $result = $conn->query($sql) or die($conn->error . __LINE__);
+
 
         if ($result->num_rows > 0) { //over 1 database(record) so run
             while ($row = $result->fetch_assoc()) {
-                $_SESSION['email'] = $row['email'];
+                $_SESSION['client_email'] = $row['email'];
             }
         }
-        $sqli = "insert into select_question values('$generateID','" . $_SESSION['email'] . "')";
-        $run = $conn->query($sqli) or die($conn->error . __LINE__);
+
+        $mysqli = "SELECT * FROM appointment where user_Email='" . $_SESSION['client_email'] . "' and appointment_status=1 or appointment_status=2 or appointment_status=5";
+        $check = $conn->query($mysqli) or die($conn->error . __LINE__);
+        $count = mysqli_num_rows($check);
+
+        if ($count > 0) {
+            echo '<script>window.alert("You must finish the current consultation first!");window.location.assign("Help.php")</script>';
+        } else {
+            $sqli = "insert into select_question values('$generateID','" . $_SESSION['client_email'] . "')";
+            $run = $conn->query($sqli) or die($conn->error . __LINE__);
+        }
     } else {
         $_SESSION['work_id'] = '';
         echo '<script>window.alert("You must login first!");window.location.assign("login.php")</script>';
@@ -49,7 +74,7 @@ if (isset($_GET['work_id'])) {
 
 
 //get the id from select_question
-$Email = $_SESSION['email'];
+$Email = $_SESSION['client_email'];
 $work_id = $_SESSION['work_id'];
 
 
@@ -94,7 +119,7 @@ if (isset($_POST['submit'])) { //if user submit the choices, go to the next ques
     $results = $conn->query($query2) or die($conn->error . __LINE__);
 
 
-    if ((!empty($_SESSION['work_id'])) && ($number == 2)){
+    if ((!empty($_SESSION['work_id'])) && ($number == 2)) {
         $number = 5;
         $query = "select * from questions where question_number='$number'";
         $result = $conn->query($query) or die($conn->error . __LINE__);
@@ -110,7 +135,6 @@ if (isset($_POST['submit'])) { //if user submit the choices, go to the next ques
                 display:block !important;            
             }</style>';
         }
-
     } else if ((!empty($_SESSION['work_id'])) && ($number >= 5)) {
         ++$number;
         $query = "select * from questions where question_number='$number'";
@@ -128,7 +152,6 @@ if (isset($_POST['submit'])) { //if user submit the choices, go to the next ques
                 display:block !important;            
             }</style>';
         }
-
     } else {
         //get the next question
         ++$number;
@@ -188,87 +211,146 @@ if (isset($_POST['direct'])) { //if user submit the choices, go to the next ques
 if (isset($_POST['finish'])) { //if done
     echo '<script>window.location.assign("showResult.php");</script>';
 }
-
 ?>
 
-
-
-
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Question</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.staticfile.org/font-awesome/4.7.0/css/font-awesome.css">
     <link rel="stylesheet" type="text/css" href="css/question.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <style>
+        body {
+            background-color: rgba(210, 232, 243) !important;
+            padding-top: 60px;
+            padding-bottom: 20px;
+        }
+    </style>
 </head>
-<!DOCTYPE html>
-<html lang="en">
+<header>
+</header>
+
 <section id="Question">
 
     <body>
-
-        <h3>Answer the questions and get matched a suitable therapist!</h3>
-        <hr id="ans">
-        <!-- Total question -->
         <div class="container">
-            <div class="question-form">
-                <form action="question.php" method="post">
-                    <p id="over">Over the past 2 weeks, how often have you bothered by any of the following problems: </p>
-                    <!-- Show the question -->
-                    <?php if ($number <= 31) {
-                    ?>
-                        <h4 class="question-text"><strong><?php echo $_SESSION['question_text'] ?></strong></h4>
-                    <?php
-                    } else { ?>
-                        <h4 class="question-text"><strong>You've completed the questionnaire!</strong></h4>
-                    <?php
-                    }
-                    ?>
-                    <hr>
-                    <ul class="choices">
-                        <!-- If the choices belongs to this question -->
-                        <?php while ($row = $choices->fetch_assoc()) : ?>
-                            <li>
-                                <div class="custom-control custom-radio">
-                                    <input type="radio" class="custom-control-input" name="choice" id="<?php echo $row['choice_id'] ?>" value="<?php echo $row['choice_id'] ?>" required checked>
-                                    <label class="custom-control-label" for="<?php echo $row['choice_id'] ?>"><strong><span><?php echo $row['text'] ?></span></strong></label>
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="col-md-10" align="center">
+                            <h2>Affordable, private online counseling in Anytime,Anywhere.</h2>
+
+                            <p>Contact with a licensed, professional therapist online.</p>
+
+                            <p>We will matched you to a suitable therapist.Get feedback, advice and guidance from your therapist!</p>
+                        </div>
+                    </div>
+
+
+                    <div class="col-md-6 question-form">
+                        <form action="question.php" method="POST">
+                            <h3>Answer the questions and get matched a suitable therapist!</h3>
+                            <hr>
+                            <p id="over">Over the past 2 weeks, how often have you bothered by any of the following problems: </p>
+                            <!-- Show the question -->
+                            <?php if ($number <= 31) {
+                            ?>
+                                <h4 class="question-text"><strong><?php echo $_SESSION['question_text'] ?></strong></h4>
+                            <?php
+                            } else { ?>
+                                <center><h4 class="question-text"><strong>You've completed the questionnaire!</strong></h4></center>
+                            <?php
+                            }
+                            ?>
+                            <hr>
+                            <ul class="choices">
+                                <!-- If the choices belongs to this question -->
+                                <?php while ($row = $choices->fetch_assoc()) : ?>
+                                    <li>
+                                        <div class="custom-control custom-radio">
+                                            <input type="radio" class="custom-control-input" name="choice" id="<?php echo $row['choice_id'] ?>" value="<?php echo $row['choice_id'] ?>" required checked>
+                                            <label class="custom-control-label" for="<?php echo $row['choice_id'] ?>"><strong><span><?php echo $row['text'] ?></span></strong></label>
+                                        </div>
+                                    </li>
+                                <?php endwhile; ?>
+                            </ul>
+
+                            <!-- If question haven't finish -->
+                            <?php if ((!empty($_SESSION['work_id'])) && ($number <= 29)) {
+                                echo "<input type=\"submit\" value=\"Next\" name=\"submit\" class=\"btn btn-outline-success\">";
+                            } else if ((!empty($_SESSION['work_id'])) && ($number == 30)) {
+                                echo "<button type=\"submit\" value=\"Finish\" name=\"direct\" class=\"btn btn-outline-success\">Finish</button>";
+                            } else if ($number <= 31) {
+                                echo "<input type=\"submit\" value=\"Next\" name=\"submit\" class=\"btn btn-outline-success\">";
+                            } else {
+                                // If done
+                                echo "<input type=\"submit\" value=\"Finish\" name=\"finish\" class=\"btn btn-outline-success\">";
+                            }
+                            ?>
+                            <!-- If cancel -->
+                            <input type="submit" value="Cancel" name="cancel" id="cancel" class="btn btn-outline-danger" onclick="return confirm('Are you sure you want to cancel?')">
+                            <!-- Current question number -->
+                            <input type="hidden" name="number" value="<?php echo $number ?>">
+                            <!-- Current id -->
+                            <input name="big_id" type="hidden" value="<?php echo $_SESSION['generate_id'] ?>">
+                            <!-- current user email -->
+                            <input name="email" type="hidden" value="<?php echo $_SESSION['client_email'] ?>">
+                        </form>
+                    </div>
+
+                    <div class="col-md-12 my-5" align="center">
+                        <div id="demo" class="carousel slide" data-ride="carousel">
+                            <ul class="carousel-indicators">
+                                <li data-target="#demo" data-slide-to="0" class="active"></li>
+                                <li data-target="#demo" data-slide-to="1"></li>
+                                <li data-target="#demo" data-slide-to="2"></li>
+                            </ul>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <center><img src="images/therapists/hair (3).png" alt="Los Angeles" width="100" height="100"></center>
+                                    <div class="carousel-caption">
+                                        <h3>Los Angeles</h3>
+                                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste labore ipsum soluta exercitationem consectetur iusto. Nobis omnis iste maxime odit delectus, quasi officia optio, nulla quos tempora pariatur, perferendis modi?</p>
+                                    </div>
                                 </div>
-                            </li>
-                        <?php endwhile; ?>
-                    </ul>
 
-                    <!-- If question haven't finish -->
-                    <?php if ((!empty($_SESSION['work_id'])) && ($number <= 29)) {
-                        echo "<input type=\"submit\" value=\"Next\" name=\"submit\" class=\"btn btn-outline-success\">";
-                    } else if ((!empty($_SESSION['work_id'])) && ($number == 30)) {
-                        echo "<button type=\"submit\" value=\"Finish\" name=\"direct\" class=\"btn btn-outline-success\" style=\"margin: 30px 0 0 475px;\">Finish</button>";
-                    } else if ($number <= 31) {
-                        echo "<input type=\"submit\" value=\"Next\" name=\"submit\" class=\"btn btn-outline-success\">";
-                    } else {
-                        // If done
-                        echo "<input type=\"submit\" value=\"Finish\" name=\"finish\" class=\"btn btn-outline-success\" style=\"margin: 30px 0 0 475px;\">";
-                    }
-                    ?>
-                    <!-- If cancel -->
-                    <input type="submit" value="Cancel" name="cancel" id="cancel" class="btn btn-outline-danger" onclick="return confirm('Are you sure you want to cancel?')">
-                    <!-- Current question number -->
-                    <input type="hidden" name="number" value="<?php echo $number ?>">
-                    <!-- Current id -->
-                    <input name="big_id" type="hidden" value="<?php echo $_SESSION['generate_id'] ?>">
-                    <!-- current user email -->
-                    <input name="email" type="hidden" value="<?php echo $_SESSION['email'] ?>">
-                </form>
+                                <div class="carousel-item">
+                                    <center><img src="images/therapists/hair (3).png" alt="Los Angeles" width="100" height="100"></center>
+                                    <div class="carousel-caption">
+                                        <h3>Los Angeles</h3>
+                                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste labore ipsum soluta exercitationem consectetur iusto. Nobis omnis iste maxime odit delectus, quasi officia optio, nulla quos tempora pariatur, perferendis modi?</p>
+                                    </div>
+                                </div>
 
+                                <div class="carousel-item">
+                                    <center><img src="images/therapists/hair (3).png" alt="Los Angeles" width="100" height="100"></center>
+                                    <div class="carousel-caption">
+                                        <h3>Los Angeles</h3>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt minima odio a aperiam deleniti eveniet iusto fuga exercitationem fugiat, magni quo temporibus totam harum quia mollitia doloribus incidunt? Fuga, ipsum.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                                <span class="carousel-control-prev-icon"></span>
+                            </a>
+                            <a class="carousel-control-next" href="#demo" data-slide="next">
+                                <span class="carousel-control-next-icon"></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <!-- jQuery library -->
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     </body>
 </section>
 
