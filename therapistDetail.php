45661924ab5f210704bf423eeedf990b337d9b8f
D190188B <68429213+D190188B@users.noreply.github.com>
2020-09-09 20:19:47 +0800

    Update therapistDetail.php

--- therapistDetail.php
+++ therapistDetail.php
@@ -9,14 +9,20 @@ if (isset($_GET['id'])) {
 
     if ($result->num_rows > 0) { //over 1 database(record) so run
         while ($row = $result->fetch_assoc()) {
-            $_SESSION['work_theraID'] = $row['therapist_id'];
-            $_SESSION['therapist_name_first'] = $row['name_first'];
-            $_SESSION['therapist_name_last'] = $row['name_last'];
-            $_SESSION['therapist_about'] = $row['about'];
-            $_SESSION['therapist_age'] = $row['age'];
-            $_SESSION['therapist_license'] = $row['license'];
-            $_SESSION['therapist_profile_image'] = $row['profile_image'];
-            $_SESSION['therapist_gender'] = $row['gender'];
+            $_SESSION['detail_work_theraID'] = $row['therapist_id'];
+            $_SESSION['detail_thera_name_first'] = $row['name_first'];
+            $_SESSION['detail_thera_name_last'] = $row['name_last'];
+            $_SESSION['detail_thera_about'] = $row['about'];
+            $_SESSION['detail_thera_gender'] = $row['gender'];
+            $_SESSION['detail_thera_address'] = $row['address'];
+            $_SESSION['detail_thera_city'] = $row['therapist_city'];
+            $_SESSION['detail_thera_postCode'] = $row['therapist_postCode'];
+            $_SESSION['detail_thera_state'] = $row['therapist_state'];
+            $_SESSOPM['detail_thera_phone'] = $row['phone'];
+            $_SESSION['detail_thera_age'] = $row['age'];
+            $_SESSION['detail_thera_email'] = $row['email'];
+            $_SESSION['detail_thera_license'] = $row['license'];
+            $_SESSION['detail_thera_profile_image'] = $row['profile_image'];
         }
     }
 }
@@ -50,17 +56,17 @@ if (isset($_GET['id'])) {
             <div class="col-md-12">
                 <div class="row">
                     <div class="col-md-12 text-center" id="theraTop">
-                        <img src="<?php echo $_SESSION['therapist_profile_image'] ?>" alt="image" id="image" name="image">
-                        <h3 class="theraName"><?php echo $_SESSION['therapist_name_first'] . "&nbsp;" . $_SESSION['therapist_name_last'] ?></h3>
-                        <h4 id="theraLic"><?php echo $_SESSION['therapist_license'] ?></h4>
+                        <img src="<?php echo $_SESSION['detail_thera_profile_image'] ?>" alt="image" id="image" name="image">
+                        <h3 class="theraName"><?php echo $_SESSION['detail_thera_name_first'] . "&nbsp;" . $_SESSION['detail_thera_name_last'] ?></h3>
+                        <h4 id="theraLic"><?php echo $_SESSION['detail_thera_license'] ?></h4>
                         <h4 class="paraTop">
-                            Age: <?php echo $_SESSION['therapist_age'] ?>
+                            Age: <?php echo $_SESSION['detail_thera_age'] ?>
                         </h4>
                         <h4 class="paraTop">
-                            Gender:<?php echo $_SESSION['therapist_gender'] ?>
+                            Gender:<?php echo $_SESSION['detail_thera_gender'] ?>
                         </h4>
 
-                        <a href="question.php?work_id=<?php echo $_SESSION['work_theraID'] ?>"><button type="submit" name="submit" id="submit" class="btn btn-outline-danger">Work with me!</button></a>
+                        <a href="question.php?work_id=<?php echo $_SESSION['detail_work_theraID'] ?>"><button type="submit" name="submit" id="submit" class="btn btn-outline-danger">Work with me!</button></a>
                         <hr>
                     </div>
 
@@ -69,7 +75,7 @@ if (isset($_GET['id'])) {
                     <div class="col-md-12" id="theraAbout">
                         <h3 class="theraAbout">ABOUT ME</h3>
                         <p class="para">
-                            <?php echo $_SESSION['therapist_about'] ?>
+                            <?php echo $_SESSION['detail_thera_about'] ?>
                         </p>
                         <hr>
                     </div>
