5cce58690660ab29d8bc7631c0b04be312900a86
D190188B <68429213+D190188B@users.noreply.github.com>
2020-09-09 20:21:31 +0800

    Update sessionTop.php

--- sessionTop.php
+++ sessionTop.php
@@ -9,16 +9,162 @@ $conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection #scawx
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 
-
+date_default_timezone_set('Singapore');
 mysqli_set_charset($conn, 'utf8');
 session_start();
 
 
 if (isset($_POST['logout'])) {
-    session_destroy();
-    header('refresh: 0; url=Home.php');
+  session_destroy();
+  header('refresh: 0; url=Home.php');
+}
+
+$date1 = date("Y-m-d");
+$appointmentNumTop = 0;
+$TodayNumTop = 0;
+$WaitNumTop = 0;
+
+
+if (isset($_SESSION['client_id'])) { //if already login
+  $id = $_SESSION['client_id'];
+
+  $sql = "select * from client where id ='$id'"; //id is database name
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) { //over 1 database(record) so run
+    while ($row = $result->fetch_assoc()) {
+      $_SESSION['client_id'] = $row['id'];
+      $_SESSION['client_name_first'] = $row['name_first'];
+      $_SESSION['client_name_last'] = $row['name_last'];
+      $_SESSION['client_email'] = $row['email'];
+      $_SESSION['client_birth'] = $row['birth'];
+      $_SESSION['client_phone'] = $row['phone'];
+      $_SESSION['client_address'] = $row['address'];
+      $_SESSION['client_city'] = $row['city'];
+      $_SESSION['client_state'] = $row['state'];
+      $_SESSION['client_post_code'] = $row['post_code'];
+      $_SESSION['client_profileImage'] = $row['profileImage'];
+    }
   }
-?>
+  $topWait = "SELECT * from appointment left join therapist on appointment.therapist_ID=therapist.therapist_id left join therastatus on appointment.appointment_status=therastatus.id where user_Email='".$_SESSION['client_email']."' and appointment_status='1' ORDER BY created_TIME DESC";
+  $getBeNum1 = $conn->query($topWait) or die($conn->error . __LINE__);
+  if ($getBeNum1->num_rows > 0) {
+    while ($row = $getBeNum1->fetch_assoc()) {
+      ++$appointmentNumTop;
+      ++$WaitNumTop;
+    }
+  }
+
+  $topMysql = "SELECT * from appointment left join therapist on appointment.therapist_ID=therapist.therapist_id left join therastatus on appointment.appointment_status=therastatus.id where user_Email='".$_SESSION['client_email']."' and user_date ='$date1' and appointment_status='2' or appointment_status='5' ORDER BY created_TIME DESC";
+  $getTodayNum1 = $conn->query($topMysql) or die($conn->error . __LINE__);
+  if ($getTodayNum1->num_rows > 0) {
+    while ($row = $getTodayNum1->fetch_assoc()) {
+      ++$appointmentNumTop;
+      ++$TodayNumTop;
+    }
+  }
+}
+
+//check the pathinfo and upload the file
+function upload_profile($path, $file)
+{
+  $targetDir = $path;
+  $default = "beard.png";
+
+  // get the filename
+  $filename = basename($file['name']);
+  $targetFilePath = $targetDir . $filename;
+  $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
+
+  if (!empty($filename)) {
+    // allow certain file format
+    $allowType = array('jpg', 'png', 'jpeg', 'gif', 'pdf');
+    if (in_array($fileType, $allowType)) {
+      // upload file to the server
+      if (move_uploaded_file($file['tmp_name'], $targetFilePath)) {
+        return $targetFilePath;
+      }
+    }
+  }
+  // return default image
+  return $path . $default;
+}
+
+
+// upload user's certificate
+function upload_certificate($path, $file)
+{
+  $targetDir = $path;
+  $default = "beard.png";
+  // get the filename
+  $filename = basename($file['name']);
+  $targetFilePath = $targetDir . $filename;
+  $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
+
+  if (!empty($filename)) {
+    // allow certain file format
+    $allowType = array('docx', 'doc', 'pptx', 'pdf', 'jpg', 'png', 'jpeg');
+    if (in_array($fileType, $allowType)) {
+      // upload file to the server
+      if (move_uploaded_file($file['tmp_name'], $targetFilePath)) {
+        return $targetFilePath;
+      }
+    }
+  } else {
+    // return default image
+    return '';
+  }
+}
+
+//upload user image
+function upload_thera($path, $file)
+{
+  $targetDir = $path;
+  $default = "beard.png";
+  // get the filename
+  $filename = basename($file['name']);
+  $targetFilePath = $targetDir . $filename;
+  $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
+
+  if (!empty($filename)) {
+    // allow certain file format
+    $allowType = array('jpg', 'png', 'jpeg', 'gif', 'pdf');
+    if (in_array($fileType, $allowType)) {
+      // upload file to the server
+      if (move_uploaded_file($file['tmp_name'], $targetFilePath)) {
+        return $targetFilePath;
+      }
+    }
+  } else {
+    // return default image
+    return $path . $default;
+  }
+}
+
+
+//prevent injection
+function validate_input_text($textValue)
+{
+  if (!empty($textValue)) {
+    $trim_text = trim($textValue);
+    // remove illegal character
+    $sanitize_str = filter_var($trim_text, FILTER_SANITIZE_STRING);
+    return $sanitize_str;
+  }
+  return '';
+}
+
+//prevent injection
+function validate_input_email($emailValue)
+{
+  if (!empty($emailValue)) {
+    $trim_text = trim($emailValue);
+    // remove illegal character
+    $sanitize_str = filter_var($trim_text, FILTER_SANITIZE_EMAIL);
+    return $sanitize_str;
+  }
+  return '';
+}
