0bb0c9e3bec8205309f55428c39676904ede8e55
D190188B <68429213+D190188B@users.noreply.github.com>
2020-09-19 20:05:43 +0800

    Update insert.php

--- insert.php
+++ insert.php
@@ -13,35 +13,80 @@ if ($conn->connect_error) {
 }
 
 $output = '';
-$search = $_POST['search'];
-$sql = "SELECT * FROM therapist where name_first LIKE '%" . $search . "%' or name_last LIKE '%" . $search . "%' and statusID='2'";
-$result = mysqli_query($conn, $sql);
+if (isset($_POST['getLicense'])) {
+    $getLicense = mysqli_real_escape_string($conn, $_POST['getLicense']);
+    if ($getLicense == 'All') {
+        $sql = "SELECT * FROM therapist where statusID='2'";
+        $result = mysqli_query($conn, $sql);
 
-$sqli = "SELECT * FROM therapist where statusID='2'";
-$run = mysqli_query($conn, $sqli);
-
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_array($result)) {
-        $output .= '<div class="col-md-4">
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_array($result)) {
+                $output .= '<div class="col-md-3">
         <a href="therapistDetail.php?id=' . $row['therapist_id'] . '">
-            <div class="card h-100 border-100">
+            <div class="card h-100 border-0">
                 <div class="card-body">
                     <img src="' . $row['profile_image'] . '" alt="image" id="therapist1">
-                    <h3 class="therapistname">' . $row['name_first'] . '&nbsp;' . $row['name_last'] . '</h3>
+                    <h4 class="therapistname">' . $row['name_first'] . '&nbsp;' . $row['name_last'] . '</h4>
                     <h5 class="therapistedu">' . $row['license'] . '</h5>
                 </div>
             </div>
         </a>
     </div>';
-    }
+            }
+            echo $output;
+        }
+    } else {
+        $sql = "SELECT * FROM therapist where license='$getLicense' and statusID='2'";
+        $result = mysqli_query($conn, $sql);
 
-    echo $output;
-} else {
-    echo $output = '<div class="col-md-4">
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_array($result)) {
+                $output .= '<div class="col-md-3">
+        <a href="therapistDetail.php?id=' . $row['therapist_id'] . '">
+            <div class="card h-100 border-0">
+                <div class="card-body">
+                    <img src="' . $row['profile_image'] . '" alt="image" id="therapist1">
+                    <h4 class="therapistname">' . $row['name_first'] . '&nbsp;' . $row['name_last'] . '</h4>
+                    <h5 class="therapistedu">' . $row['license'] . '</h5>
+                </div>
+            </div>
+        </a>
+    </div>';
+            }
+            echo $output;
+        }
+    }
+}
+
+
+if (isset($_POST['search'])) {
+    $search = mysqli_real_escape_string($conn, $_POST['search']);
+    $sql = "SELECT * FROM therapist where name_first LIKE '%" . $search . "%' or name_last LIKE '%" . $search . "%' or license LIKE '%" . $search . "%' and statusID='2'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_array($result)) {
+            $output .= '<div class="col-md-3">
+        <a href="therapistDetail.php?id=' . $row['therapist_id'] . '">
+            <div class="card h-100 border-0">
+                <div class="card-body">
+                    <img src="' . $row['profile_image'] . '" alt="image" id="therapist1">
+                    <h4 class="therapistname">' . $row['name_first'] . '&nbsp;' . $row['name_last'] . '</h4>
+                    <h5 class="therapistedu">' . $row['license'] . '</h5>
+                </div>
+            </div>
+        </a>
+    </div>';
+        }
+
+        echo $output;
+    } else {
+        echo $output .= '<div class="col-md-4">
         <div class="card h-100 border-0">
             <div class="card-body">
                 <h2>There is no result!</h2>
             </div>
         </div>
 </div>';
+    }
 }
