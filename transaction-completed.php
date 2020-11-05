a7c6e443fd1429f8734cabe18b065d496bf85d77
D190188B <68429213+D190188B@users.noreply.github.com>
2020-11-05 23:01:09 +0800

    Update transaction-completed.php

--- transaction-completed.php
+++ transaction-completed.php
@@ -6,8 +6,10 @@ require __DIR__ . '/vendor/autoload.php';
 //1. Import the PayPal SDK client that was created in `Set up Server-Side SDK`.
 use Sample\PayPalClient;
 use PayPalCheckoutSdk\Orders\OrdersGetRequest;
+
 require 'paypal-client.php';
-$orderID=$_GET['orderID'];
+
+$orderID = $_GET['orderID'];
 
 class GetOrder
 {
@@ -24,32 +26,38 @@ class GetOrder
     $response = $client->execute(new OrdersGetRequest($orderId));
 
     //transaction details
-    $orderID= $response->result->id;
+    $receiptID = $response->result->id;
     $email = $response->result->payer->email_address;
     $name = $response->result->purchase_units[0]->shipping->name->full_name;
-    $address1 = $response->result->purchase_units[0]->shipping->address->address_line_1;
-    $address2 = $response->result->purchase_units[0]->shipping->address->admin_area_2;
-    $address3 = $response->result->purchase_units[0]->shipping->address->admin_area_1;
-    $address4 = $response->result->purchase_units[0]->shipping->address->postal_code;
-    $address5 = $response->result->purchase_units[0]->shipping->address->country_code;
-    $FullAddress= $address1.",".$address4."&nbsp".$address2.",".$address3.",".$address5;
+    //   purchase_units: [{
+    //     amount: {
+    //         value: Amount
+    //     }
+    // }],
+    $amount = $response->result->purchase_units[0]->amount->value;
+
+
 
     //include details to our database
-    include('sessionTop.php');
+    include("sessionTop.php");
+    $appointmentID = $_SESSION['pay_appointmentID'];
 
-    $payment="INSERT INTO payments (name, email, orderID, address) VALUE ('$name','$email','$orderID','$FullAddress')";
-    $runPay=$conn->query($payment) or die($conn->error.__LINE__);
 
-    if($runPay == false){
-        echo "There was a problem on your code".mysqli_error($conn);
-    }else{
-        header("Location:Home.php");
+    $payment = "INSERT INTO payments (name, email, receiptID, amount, appointmentID, created_time) VALUE ('$name','$email','$receiptID','$amount','$appointmentID',NOW())";
+    $runPay = $conn->query($payment) or die($conn->error . __LINE__);
+
+    if ($runPay == false) {
+      echo "There was a problem on your code" . mysqli_error($conn);
+    } else {
+      $payment = "UPDATE appointment set paymentStatus='2'";
+      $runPay = $conn->query($payment) or die($conn->error . __LINE__);
+      $_SESSION['receipt_ID'] = $receiptID;
+      echo '<script>window.location.assign("receipt.php")</script>';
+      // header('url=Home.php');
     }
   }
 }
 
-if (!count(debug_backtrace()))
-{
+if (!count(debug_backtrace())) {
   GetOrder::getOrder($orderID, true);
 }
-?>
