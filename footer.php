c693d2a765b771a3b50c4d5a0f106b48923c4665
D190188B <68429213+D190188B@users.noreply.github.com>
2020-11-05 22:56:11 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,38 +1,35 @@
+<footer class="container py-5" id="footer" align="center">
+  <div class="row">
+    <div class="col-6 col-md-3">
+      <h5>About us</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="text-muted" href="#"></a></li>
+        <li><a class="text-muted" href="#"></a></li>
+        <li><a class="text-muted" href="#"></a></li>
+        <li><a class="text-muted" href="#"></a></li>
+        <li><a class="text-muted" href="#"></a></li>
+      </ul>
+    </div>
 
-    <footer class="container py-5" id="footer" >
-                    
-                    <div class="row" >
+    <div class="col-6 col-md-3">
+      <h5>Support</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="text-muted" href="feedback.php">Feedback</a></li>
+        <li><a class="text-muted" href="#">FAQ</a></li>
+        <li><a class="text-muted" href="#">Reviews</a></li>
+      </ul>
+    </div>
 
-                      <div class="col-12 col-md">
-                        <h5>About us</h5>
-                        <ul class="list-unstyled text-small">
-                          <li><a class="text-muted" href="#"></a></li>
-                          <li><a class="text-muted" href="#"></a></li>
-                          <li><a class="text-muted" href="#"></a></li>
-                          <li><a class="text-muted" href="#"></a></li>
-                          <li><a class="text-muted" href="#"></a></li>
-                        </ul>
-                      </div>
-           
-                      <div class="col-6 col-md">
-                        <h5>Support</h5>
-                        <ul class="list-unstyled text-small">
-                          <li><a class="text-muted" href="#">Feedback</a></li>
-                          <li><a class="text-muted" href="#">FAQ</a></li>
-                          <li><a class="text-muted" href="#">Reviews</a></li>
-                        </ul>
-                      </div>
+    <div class="col-6 col-md-3">
+      <h5>Our therapists</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="text-muted" href="#">Therapists information</a></li>
+        <li><a class="text-muted" href="#">Apply Job</a></li>
+    </div>
 
-                      <div class="col-6 col-md">
-                        <h5>Our therapists</h5>
-                        <ul class="list-unstyled text-small">
-                          <li><a class="text-muted" href="#">Therapists information</a></li>
-                          <li><a class="text-muted" href="#">Apply Job</a></li>
-                      </div>
+    <div class="col-6 col-md col-md-3">
+      <small class="d-block mb-3 text-muted">&copy;2020 Southern IT-19B</small>
+    </div>
 
-                      <div class="col-6 col-md col-md">
-                        <small class="d-block mb-3 text-muted">&copy;2020 Southern IT-19B</small>
-                      </div>
-                      
-                    </div>
-                  </footer>
+  </div>
+</footer>
