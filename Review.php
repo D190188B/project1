c7c4409316e295db351a519e46ac0fc100da6a13
D190188B <68429213+D190188B@users.noreply.github.com>
2020-11-12 22:46:09 +0800

    Update Review.php

--- Review.php
+++ Review.php
@@ -1,5 +1,9 @@
 <?php
-include ("sessionTop.php");
+include("sessionTop.php");
+
+$sql = "SELECT * FROM review left join therapist on review.Therapist_ID=therapist.therapist_id ORDER BY Created_Time ASC";
+$run = $conn->query($sql) or die($conn->error . __LINE__);
+
 ?>
 
 <!doctype html>
@@ -10,11 +14,11 @@ include ("sessionTop.php");
     <link rel="stylesheet" type="text/css" href="css/Reviews.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    
+
     <style>
         #reviews {
             background-image: url('images/twinkle.jpg');
-            background-size: 100% 100%;
+            background-size: cover;
         }
     </style>
 
@@ -28,13 +32,11 @@ include ("sessionTop.php");
 <section id="reviews">
 
     <div class="logo-header">
-        <div class=" h-100">
-            <div class="row h-100 align-items-center">
-                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 text-center header-content">
-                    <img src="https://i.imgur.com/A0t1a3D.png" class="img-fluid logo" alt="logo">
-                    <h1 style="color:white">Let's leave some review for us</h1>
-                    <p>Your comment make our improve.</p>
-                </div>
+        <div class="align-items-center">
+            <div class="col-md-12 text-center header-content" style="padding-top:100px;">
+                <img src="https://i.imgur.com/A0t1a3D.png" class="img-fluid logo" alt="logo">
+                <h1 style="color:white">Let's leave some review for us</h1>
+                <p>These are reviews for therapists that work with Care and Healing</p>
             </div>
         </div>
     </div>
@@ -42,170 +44,64 @@ include ("sessionTop.php");
     <body>
         <div class="container">
             <div class="row">
-                <div class="col-md-12 gedf-main">
-                    <!--- \\\\\\\Post-->
-                    <div class="card gedf-card">
-                        <div class="card-header">
-                            <ul class="nav nav-tabs card-header-tabs" id="myTab" role="tablist">
-                                <li class="nav-item">
-                                    <a class="nav-link active" id="posts-tab" data-toggle="tab" href="#posts" role="tab" aria-controls="posts" aria-selected="true">
-                                        Share your reviews</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="card-body">
-                            <div class="tab-content" id="myTabContent">
-                                <div class="tab-pane fade show active" id="posts" role="tabpanel" aria-labelledby="posts-tab">
-                                    <div class="form-group">
-                                        <label class="sr-only" for="message">post</label>
-                                        <textarea class="form-control" id="message" rows="3" placeholder="What are you thinking?"></textarea>
-                                        <select>
-                                            <option value="1">1</option>
-                                            <option value="2">2</option>
-                                            <option value="3">3</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="btn-toolbar justify-content-between">
-                                <div class="btn-group">
-                                    <button type="submit" class="btn btn-primary">share</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Post /////-->
-
                 <!--- \\\\\\\Post-->
-                <div class="col-md-6 gedf-main">
-                    <div class="card gedf-card">
-                        <div class="card-header">
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="mr-2">
-                                        <img class="rounded-circle" width="45" src="https://picsum.photos/50/50" alt="">
-                                    </div>
-                                    <div class="ml-2">
-                                        <div class="h5 m-0">@LeeCross</div>
-                                        <div class="h7 text-muted">Miracles Lee Cross</div>
-                                    </div>
-                                </div>
-                            </div>
+                <?php
+                if ($run->num_rows > 0) {
+                    while ($row = $run->fetch_assoc()) {
+                        $review_ID = $row['review_ID'];
+                        $Appointment_ID = $row['Appointment_ID'];
+                        $review = $row['review'];
+                        $client_Name = $row['client_Name'];
 
-                        </div>
-                        <div class="card-body">
-                            <div class="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i>10 min ago</div>
-                            <a class="card-link" href="#">
-                                <h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adip.</h5>
-                            </a>
+                        $user_time1 = strtotime($row['Created_Time']);
+                        $name = $row['name_first'] . " " . $row['name_last'];
 
-                            <p class="card-text">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo recusandae nulla rem eos ipsa praesentium esse magnam nemo dolor
-                                sequi fuga quia quaerat cum, obcaecati hic, molestias minima iste voluptates.
-                            </p>
-                        </div>
-                    </div>
-                </div>
+                        $Time = date("h:i a", $user_time1);
+                        $Date = date("Y-m-d", $user_time1);
 
-                <!--- \\\\\\\Post-->
-                <div class="col-md-6 gedf-main">
-                    <div class="card gedf-card">
-                        <div class="card-header">
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="mr-2">
-                                        <img class="rounded-circle" width="45" src="https://picsum.photos/50/50" alt="">
-                                    </div>
-                                    <div class="ml-2">
-                                        <div class="h5 m-0">@LeeCross</div>
-                                        <div class="h7 text-muted">Miracles Lee Cross</div>
+                        $image=$row['profile_image'];
+                ?>
+
+                        <div class="col-md-6 gedf-main">
+                            <div class="card gedf-card">
+                                <div class="card-header">
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <div class="ml-2">
+                                                <div class="h5 m-0">Review ID:# <?php echo $review_ID ?></div>
+                                                <div class="h7 text-muted">Date of review: <?php echo $Date . " " . $Time ?></div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            <div class="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i>10 min ago</div>
-                            <a class="card-link" href="#">
-                                <h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adip.</h5>
-                            </a>
 
-                            <p class="card-text">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo recusandae nulla rem eos ipsa praesentium esse magnam nemo dolor
-                                sequi fuga quia quaerat cum, obcaecati hic, molestias minima iste voluptates.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <!-- Post /////-->
+                                <div class="card-body">
+                                    <h5 class="card-title">Review written by user <?php echo $client_Name ?> after counseling with Dr. <?php echo $name ?> for 3 weeks on issues concerning </h5>
 
-                <!--- \\\\\\\Post-->
-                <div class="col-md-6 gedf-main">
-                    <div class="card gedf-card">
-                        <div class="card-header">
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="mr-2">
-                                        <img class="rounded-circle" width="45" src="https://picsum.photos/50/50" alt="">
-                                    </div>
-                                    <div class="ml-2">
-                                        <div class="h5 m-0">@LeeCross</div>
-                                        <div class="h7 text-muted">Miracles Lee Cross</div>
-                                    </div>
+                                    <p class="card-text">
+                                        <?php echo $review ?>
+                                    </p>
                                 </div>
-                            </div>
 
-                        </div>
-                        <div class="card-body">
-                            <div class="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i>10 min ago</div>
-                            <a class="card-link" href="#">
-                                <h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adip.</h5>
-                            </a>
-
-                            <p class="card-text">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo recusandae nulla rem eos ipsa praesentium esse magnam nemo dolor
-                                sequi fuga quia quaerat cum, obcaecati hic, molestias minima iste voluptates.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <!-- Post /////-->
-
-                <!--- \\\\\\\Post-->
-                <div class="col-md-6 gedf-main">
-                    <div class="card gedf-card">
-                        <div class="card-header">
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="mr-2">
-                                        <img class="rounded-circle" width="45" src="https://picsum.photos/50/50" alt="">
-                                    </div>
-                                    <div class="ml-2">
-                                        <div class="h5 m-0">@LeeCross</div>
-                                        <div class="h7 text-muted">Miracles Lee Cross</div>
+                                <div class="card-header">
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <div class="mr-2">
+                                                <img class="rounded-circle" width="45" src="<?php echo $image?>" alt="image">
+                                            </div>
+                                            <div class="ml-2">
+                                                <div class="h5 m-0">@<?php echo $name?></div>
+                                                <div class="h7 text-muted">Miracles Lee Cross</div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="card-body">
-                            <div class="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i>10 min ago</div>
-                            <a class="card-link" href="#">
-                                <h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adip.</h5>
-                            </a>
-
-                            <p class="card-text">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo recusandae nulla rem eos ipsa praesentium esse magnam nemo dolor
-                                sequi fuga quia quaerat cum, obcaecati hic, molestias minima iste voluptates.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <!-- Post /////-->
-
-
-
-
-
+                <?php
+                    }
+                }
+                ?>
             </div>
         </div>
 
