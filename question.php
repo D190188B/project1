880a1c609ccd52d831f29b311e25e8ea26ce597e
D190188B <68429213+D190188B@users.noreply.github.com>
2020-12-04 10:35:34 +0800

    Update question.php

--- question.php
+++ question.php
@@ -84,6 +84,7 @@ if (isset($_POST['finish1'])) { //if finish without the recommendation question
 <html lang="en">
 
 <head>
+    <link rel="icon" href="images/Logo.jpg" type="image/x-icon" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Question</title>
